e442528a8f1d50921c034b959a4039e8189e0002
TheNumberGuy <tiagorocha1515@gmail.com>
2019-12-13 19:43:54 +0000

    Função move de player adicionada

--- teste.js
+++ teste.js
@@ -1,5 +1,11 @@
 var jogo = [['-', '|', '-', '|', '-'], ['-', '|', '-', '|', '-'], ['-', '|', '-', '|', '-']];
 
+var player1 = {
+  move(linha, coluna){
+    jogo[linha][coluna] = 'X';
+  }
+}
+
 function render(){
   console.log(jogo);
 }
@@ -7,25 +13,28 @@ function render(){
 render();
 
 console.log('Primeira jogada:');
-jogo[0][0] = 'X';
+player1.move(0,0);
 jogo[2][4] = 'O';
 
 render();
 
 console.log('Segunda jogada:');
-jogo[2][0] = 'X';
+player1.move(2,0);
+//jogo[2][0] = 'X';
 jogo[1][0] = 'O';
 
 render();
 
 console.log('Terceira jogada:');
-jogo[0][4] = 'X';
+player1.move(0,4);
+//jogo[0][4] = 'X';
 jogo[0][2] = 'O';
 
 render();
 
 console.log('Quarta jogada:');
-jogo[1][2] = 'X';
+player1.move(1,2);
+//jogo[1][2] = 'X';
 jogo[1][4] = 'O';
 
 render();
