f9ad9140d44e84e265d1e429dad3d1c2326607be
TheNumberGuy <tiagorocha1515@gmail.com>
2019-12-13 19:39:08 +0000

    Função render adicionada

--- teste.js
+++ teste.js
@@ -1,34 +1,38 @@
 var jogo = [['-', '|', '-', '|', '-'], ['-', '|', '-', '|', '-'], ['-', '|', '-', '|', '-']];
 
-console.log(jogo);
+function render(){
+  console.log(jogo);
+}
+
+render();
 
 console.log('Primeira jogada:');
 jogo[0][0] = 'X';
 jogo[2][4] = 'O';
 
-console.log(jogo);
+render();
 
 console.log('Segunda jogada:');
 jogo[2][0] = 'X';
 jogo[1][0] = 'O';
 
-console.log(jogo);
+render();
 
 console.log('Terceira jogada:');
 jogo[0][4] = 'X';
 jogo[0][2] = 'O';
 
-console.log(jogo);
+render();
 
 console.log('Quarta jogada:');
 jogo[1][2] = 'X';
 jogo[1][4] = 'O';
 
-console.log(jogo);
+render();
 
 console.log('Fim do jogo!');
 jogo[0][4] = '/';
 jogo[1][2] = '/';
 jogo[2][0] = '/';
 
-console.log(jogo);
+render();
